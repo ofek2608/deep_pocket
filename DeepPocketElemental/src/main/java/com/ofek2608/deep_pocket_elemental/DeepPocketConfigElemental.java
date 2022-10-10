700df4f0b24e1fafa336534d9b65062e4ca65dd5
Ofek Nar <ofek2608@gmail.com>
2022-10-10 10:25:26 +0300

    Fixed few minor issues

--- DeepPocketElemental/src/main/java/com/ofek2608/deep_pocket_elemental/DeepPocketConfigElemental.java
+++ DeepPocketElemental/src/main/java/com/ofek2608/deep_pocket_elemental/DeepPocketConfigElemental.java
@@ -11,8 +11,7 @@ public final class DeepPocketConfigElemental {
 	@SuppressWarnings("EmptyMethod") public static void loadClass() {}
 	static {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Client.SPEC);
-		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Common.SPEC);
-
+//		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Common.SPEC);
 	}
 
 	public static class Client {
@@ -25,7 +24,7 @@ public final class DeepPocketConfigElemental {
 		static {
 			var builder = new ForgeConfigSpec.Builder();
 
-			builder.comment("Where to put the overlay", "Possible values are: " + Stream.of(OverlayLocation.values()).map(OverlayLocation::name).toList());
+			builder.comment("Where to put the overlay");
 			OVERLAY_LOCATION = builder.defineEnum("OverlayPosition", OverlayLocation.TOP_LEFT);
 
 			builder.comment("If set to true, icons will be on the left, otherwise on the right.");
@@ -35,15 +34,15 @@ public final class DeepPocketConfigElemental {
 		}
 	}
 
-	public static final class Common {
-		private Common() {}
-
-		private static final ForgeConfigSpec SPEC;
-
-		static {
-			var builder = new ForgeConfigSpec.Builder();
-
-			SPEC = builder.build();
-		}
-	}
+//	public static final class Common {
+//		private Common() {}
+//
+//		private static final ForgeConfigSpec SPEC;
+//
+//		static {
+//			var builder = new ForgeConfigSpec.Builder();
+//
+//			SPEC = builder.build();
+//		}
+//	}
 }
